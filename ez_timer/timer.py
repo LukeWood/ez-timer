8472a957ce9e93d04096b7c8e9cf432e7b223d74
Kartik Sharma <kartik4949@gmail.com>
2022-03-29 00:56:46 +0530

    more feats

--- ez_timer/timer.py
+++ ez_timer/timer.py
@@ -1,7 +1,28 @@
 import time
 
-class Timer(float):
-    def __init__(self, performance=True, verbose = False):
+_TIME_DECIMAL = 6
+
+
+class TimeMixin:
+    @staticmethod
+    def _ms(t):
+        return round(t * 1000, _TIME_DECIMAL)
+
+    @staticmethod
+    def _h(t):
+        return round(t / 3600, _TIME_DECIMAL)
+
+    @staticmethod
+    def _m(t):
+        return round(t / 60, _TIME_DECIMAL)
+
+    @staticmethod
+    def _hms(t):
+        return time.strftime("%H:%M:%S", time.gmtime(t))
+
+
+class Timer(float, TimeMixin):
+    def __init__(self, performance=True, verbose=False):
         self.start_time = None
         self.end_time = None
         self._elasped = 0.0
@@ -24,17 +45,41 @@ class Timer(float):
         self._elasped = self.end_time - self.start_time
 
         if self.verbose:
-            print('Time elasped: %fs' % self._elasped)
+            print("Time elasped: %fs" % self._elasped)
 
-    @property
-    def result(self):
+    def _check_runtime_error(self):
         if self.start_time is None or self.end_time is None:
             raise RuntimeError(
                 "Attempting to use timer before timing context is exited."
             )
-        return self._elasped
+
+    @property
+    def elasped(self):
+        self._check_runtime_error()
+        return self
+
+    @property
+    def hours(self):
+        self._check_runtime_error()
+        return self._h(self._elasped)
+
+    @property
+    def milliseconds(self):
+        self._check_runtime_error()
+        return self._ms(self._elasped)
+
+    @property
+    def hms(self):
+        self._check_runtime_error()
+        return self._hms(self._elasped)
+
+    @property
+    def minutes(self):
+        self._check_runtime_error()
+        return self._m(self._elasped)
 
     def __float__(self):
+        self._check_runtime_error()
         return float(self._elasped)
 
     def __str__(self):
